d1d1d609e3c3df8f02d255f0be48995841afec12
Wilbert Caba <wilbertcaba@gmail.com>
2023-03-01 15:26:56 -0500

    Momentary padding to all content wrapper

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Navbar from './Navbar';
-import styles from '../styles/Home.module.css';
 
 const Layout = ({children}) => {
   return (
     <div>
         <Navbar />
-        <div className={styles.container}> {children} </div>
+        <div className='p-4'> {children} </div>
     </div>
   )
 }
