845ceace49807a0d45b65bf6775f0a35cf26077a
Wilbert Caba <wilbertcaba@gmail.com>
2023-03-01 15:26:23 -0500

    Navbar: removed and updated links

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Link from 'next/link';
 import styles from '../styles/Navbar.module.css';
 
+
 const Navbar = () => {
   return (
     <nav>
         <div className={styles.navbar}>
-            <Link href='/'>Home</Link>
             <Link href='/coins'>Coins</Link>
-            <Link href='/about'>About</Link>
+            <Link href='/watchlist'>Watchlist</Link>
             <Link href='/profile/username'>Profile</Link>
         </div>
     </nav>
